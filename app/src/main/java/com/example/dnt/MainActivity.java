baa6eb5b240cb4544e52dc747ab87101df8b15a5
gleaming0 <aheun1216@naver.com>
2022-06-16 04:21:10 +0900

    change home fragment

---
+++ app/src/main/java/com/example/dnt/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.dnt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+
+public class MainActivity extends AppCompatActivity {
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    homeFragment homeFragment = new homeFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("userName");
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout, homeFragment).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnavi);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                switch(item.getItemId()){
+                    case R.id.home:
+                        if(homeFragment == null){
+                            homeFragment = new homeFragment();
+                            fragmentManager.beginTransaction().add(R.id.framelayout, homeFragment).commit();
+
+                        }else{
+                            fragmentManager.beginTransaction().show(homeFragment).commit();
+                        }
+                        break;
+                    case R.id.chat:
+                        break;
+                    case R.id.mypage:
+                        Intent homeIntent = new Intent(MainActivity.this, MypageActivity.class);
+                        homeIntent.putExtra("userName", userName);
+                        startActivity(homeIntent);
+                        finish();
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/dnt/homeFragment.java
@@ -0,0 +1,146 @@
+package com.example.dnt;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.dnt.PostInfo;
+import com.example.dnt.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import com.example.dnt.AddPostActivity;
+import com.example.dnt.R;
+import com.example.dnt.PostAdapter;
+import com.example.dnt.RecyclerAdapter;
+//import com.example.dnt.ImageViewPagerAdapter;
+//import com.example.dnt.RecyclerAdapter;
+//import com.example.dnt.SearchActivity;
+import com.example.dnt.PostInfo;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+public class homeFragment extends Fragment {
+    private RecyclerView recyclerView;
+    //private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<PostInfo> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    RecyclerAdapter adapter;
+
+    Button add_btn;
+    SwipeRefreshLayout swipeRefreshLayout;
+    LinearLayout forPopup;
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
+        //popup 만들기
+        forPopup = view.findViewById(R.id.forPopup);
+//        forPopup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //데이터 담아서 팝업 호출
+//                Intent intent = new Intent(this, PopupActivity.class);
+//                intent.putExtra("data", "Test Popup");
+//                startActivity(intent, 1);
+//            }
+//        });
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.homeRecyclerView);
+        recyclerView.setHasFixedSize(true); // 리사이클러뷰 기존성능 강화
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>(); // User 객체를 담을 어레이 리스트 (어댑터쪽으로)
+
+        database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
+        databaseReference = database.getReference("posts"); // DB 테이블 연결
+
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
+                    PostInfo PostInfo = snapshot.getValue(PostInfo.class); // 만들어뒀던 User 객체에 데이터를 담는다.
+                    arrayList.add(PostInfo); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+                }
+                adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침해야 반영이 됨
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 디비를 가져오던중 에러 발생 시
+                Log.e("homeFragment", String.valueOf(databaseError.toException())); // 에러문 출력
+            }
+        });
+        adapter = new RecyclerAdapter(arrayList, getContext());
+        recyclerView.setAdapter(adapter); // 리사이클러뷰에 어댑터 연결
+
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                        arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
+                            PostInfo PostInfo = snapshot.getValue(PostInfo.class); // 만들어뒀던 User 객체에 데이터를 담는다.
+                            arrayList.add(PostInfo); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+                        }
+                        adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침해야 반영이 됨
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // 디비를 가져오던중 에러 발생 시
+                        Log.e("homeFragment", String.valueOf(databaseError.toException())); // 에러문 출력
+                    }
+                });
+                adapter = new RecyclerAdapter(arrayList,getContext());
+                recyclerView.setAdapter(adapter); // 리사이클러뷰에 어댑터 연결
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+
+        //add 버튼
+        add_btn = view.findViewById(R.id.home_add_post_btn);
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AddPostActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        return view;
+    }
+}
